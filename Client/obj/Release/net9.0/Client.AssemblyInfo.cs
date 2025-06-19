d5cb54301f5fcbff0b557ea80b9ab2cd2d8dde68
marabella070 <marabella070@gmail.com>
2025-06-19 13:21:44 +0700

    Changes:
    	- Changed the CONSOLE_CLIENT constant only for the Debug version

--- Client/obj/Release/net9.0/Client.AssemblyInfo.cs
+++ Client/obj/Release/net9.0/Client.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e0c16d14eb7827a9b3d363302817c0a8434435")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
