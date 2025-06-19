e76238edb59f700f1a03ec02a98a414521e41d3f
marabella070 <marabella070@gmail.com>
2025-06-19 12:41:42 +0700

    Forgot to build the Client, again..

--- Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net9.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f166df96312edc781d6a9f0ea9faec75c277865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15be8dc4e9b8e11e0d5a36711e894213528f8b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
